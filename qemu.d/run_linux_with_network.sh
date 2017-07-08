6469bfb0a87e44a2ada74dc985ac97cf1a1262e5
aggresss <aggresss@163.com>
2017-07-08 14:23:30 +0800

    20170708A

---
+++ qemu.d/run_linux_with_network.sh
@@ -0,0 +1,12 @@
+qemu-system-arm \
+    -nographic \
+    -sd vexpress.img \
+    -M vexpress-a9 \
+    -m 512M \
+    -kernel zImage \
+    -dtb vexpress-v2p-ca9.dtb \
+    -smp 4 \
+    -append "init=/linuxrc root=/dev/mmcblk0p1 rw rootwait earlyprintk console=ttyAMA0" \
+    -net nic,vlan=0 \
+    -net tap,vlan=0,ifname=tap30,script=no,downscript=no
+
